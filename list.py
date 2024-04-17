dfc1cba5dba78492c8225e4c604efa846f271020
Madhusmita Barik <162332770+Madhusmita7894@users.noreply.github.com>
2024-04-16 20:19:24 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,46 @@
+# alphabets=["a","e","i","o","u"]
+# alphabets.sort()
+# print(alphabets)
+# #ex:-(i)
+# list=[1,2,3,4,5,6]
+# list.sort()
+# print("largest element is :",list[-1])
+#ex:-(ii)
+# my_list=[1,2,2,3,4,4,5]
+# new_list=[]
+# for item in my_list:
+#     if item not in new_list:
+#         new_list.append(item)
+# print(new_list)
+#ex:-(iii)
+# my_list=[1,2,3,4,5]
+# reversed_list=my_list[::-1]
+# print(reversed_list)
+#ex:-(iv)
+# my_list=[1,2,3,4,5]
+# element=3
+# if element in my_list:
+#     print("element exists in the list!")
+# else:
+#     print("element does not exists in the list.")
+#ex:-(v)
+# my_list=[1,2,3,4,5]
+# total=sum(my_list)
+# print(total)
+#ex:-(vi)
+# my_list=[2,3,4,5]
+# product=1
+# for num in my_list:
+#     product*=num
+# print(product)
+#ex:-(vii)
+# list1=[4,2,6]
+# list2=[1,3,5]
+# merged_list=list1 + list2
+# sorted_list=sorted(merged_list)
+# print(sorted_list)
+
+#ex:-(viii)
+# my_list=[5,2,8,1,9]
+# smallest_number=min(my_list)
+# print(smallest_number)
